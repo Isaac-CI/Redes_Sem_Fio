32d301f84ca503146f4bd380cb184bd0eccf6b3e
msrodrigues <miguel.rodrigues@synchro.com.br>
2023-06-03 19:24:25 -0300

    changing run.sh comments

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 # First, you'll have to export NS3_DIR to the ns-3.38 directory in your machine
 # run export NS3_DIR=<path-to-ns3>
 
+# Then, give the script, permission to execute
+# chmod +x run.sh
+
 # 1. Copying the src folder to the NS3 directory
 cp -r src/ ${NS3_DIR}/scratch
 
